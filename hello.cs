9d5640178a90c25421cce0dbdb43d1ad3cccfb57
“Andrew <abiv23@gmail.com>
2017-02-01 11:09:07 -0700

    :tada: hello world working

---
+++ hello.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello, world");
+            Console.ReadKey();
+        }
+    }
+}
